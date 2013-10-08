4ee514035402effbb9f4eda965af592815ff107b
KoffiePatje_Laptop <koffiepatje@gmail.com>
2013-10-09 00:01:32 +0200

    Framework

--- src/nl/pleduc/mc/CommandEdit/CommandEditListener.java
+++ src/nl/pleduc/mc/CommandEdit/CommandEditListener.java
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 // ------------------------------------------------------------------------ //
 //                              File Reference                              //
 // Author: Patrick le Duc                                                   //
-<<<<<<< HEAD
 // Filename: CommandEditListener.java                                       //
 // License: GNU GPLv2                                                       //
 // Description:                                                             //
 // -- SOMETHING --                                                          //
-=======
-// Filename: CommandEditListener.java                                                   //
-// License: GNU GPLv2                                                       //
-// Description:                                                             //
-// -- SOMETHING --                                                           //
->>>>>>> 8aceaaa1fec98e84cde622414f9ab079ca04f048
 //                                                                          //
 // ------------------------------------------------------------------------ //
 
 package nl.pleduc.mc.CommandEdit;
 
-<<<<<<< HEAD
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -41,27 +32,4 @@ public class CommandEditListener implements Listener
         
         m_Base.getLogger().info( "Command Edit catched the following command: " + m );
     }
-=======
-
-public class CommandEditListener {
-
->>>>>>> 8aceaaa1fec98e84cde622414f9ab079ca04f048
 }
-=======
-// ------------------------------------------------------------------------ //
-//                              File Reference                              //
-// Author: Patrick le Duc                                                   //
-// Filename: CommandEditListener.java                                                   //
-// License: GNU GPLv2                                                       //
-// Description:                                                             //
-// -- SOMETHING --                                                           //
-//                                                                          //
-// ------------------------------------------------------------------------ //
-
-package nl.pleduc.mc.CommandEdit;
-
-
-public class CommandEditListener {
-
-}
->>>>>>> 8aceaaa1fec98e84cde622414f9ab079ca04f048
